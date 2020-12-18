44a7db8358e050feb1f0ea8ce612b9a8135ea582
sonam verma <rajputsonam1408@gmail.com>
2020-12-18 22:20:05 +0530

    counter App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counter-app';
+  count: number = 0;
+
+  onIncrease(){
+    if (this.count === 10){
+      this.count = 0;
+    }
+    this.count = this.count + 1;
+  }
+  onDecrease(){
+    if (this.count === 0){
+      this.count = this.count + 1;
+    }
+    this.count = this.count - 1;
+  }
+  onReset(){
+    this.count = 0;
+  }
 }
